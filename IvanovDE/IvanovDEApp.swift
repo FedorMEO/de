8c039ce0fba3de6bdefe47c558cfb896cf01ccb5
Marina2204 <Mariska22mak22@yandex.ru>
2024-10-12 09:37:19 +0700

    Initial Commit

---
+++ IvanovDE/IvanovDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  IvanovDEApp.swift
+//  IvanovDE
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct IvanovDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
